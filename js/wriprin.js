d7635e0be5380cc02796d7f2089bcec2b1b4e45a
Wriprin <wriprin@gmail.com>
2021-09-12 10:27:56 +0800

    Site updated: 2021-09-12 10:27:49

--- js/wriprin.js
+++ js/wriprin.js
@@ -1,7 +1,7 @@
 $(function(){
  
 /*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+var top_up = "<img id='upj' class='upj' style='max-width: 500%; transform: translate(-70px,-70px); opacity: 1.0;' src='https://cdn.jsdelivr.net/gh/Wriprin/Wriprin-Gallery/Gallery/【GIF】3301.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 
